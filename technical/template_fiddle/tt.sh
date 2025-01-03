4648d56b603d3021dd2bd9cac98aa0622195d419
Peter Yoachim <yoachim@uw.edu>
2025-01-03 10:38:11 -0800

    trya again

--- technical/template_fiddle/tt.sh
+++ technical/template_fiddle/tt.sh
@@ -3,3 +3,4 @@ python template_time.py --season_length 365
 python template_time.py --season_length 425
 python template_time.py --season_length 547
 python template_time.py --season_length 730
+python baseline.py
